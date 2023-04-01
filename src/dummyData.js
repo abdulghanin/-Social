cd4429bf55f1fcc9360d1e06b55a59a77ee2f3ad
abdulghani <abdulghani7827@gmail.com>
2023-04-01 05:28:43 -0700

    socail useing react

---
+++ src/dummyData.js
@@ -0,0 +1,48 @@
+export const Users = [
+  {
+    id:1,
+    profilePicture: "assets/person/1.jpg",
+    username: "Abdulghani",
+  },
+  {
+    id:2,
+    profilePicture: "assets/person/2.jpg",
+    username: "Faisal",
+  },
+  {
+    id:3,
+    profilePicture: "assets/person/3.jpg",
+    username: "abahlu",
+  }
+ 
+];
+
+export const Posts = [
+  {
+    id: 1,
+    desc: "Love For All, Hatred For None.",
+    photo: "assets/post/1.jpg",
+    date: "5 mins ago",
+    userId: 1,
+    like: 32,
+    comment: 9,
+  },
+  {
+    id: 2,
+    desc: "Love For All,",
+    photo: "assets/post/sudan4.jpg",
+    date: "5 mins ago",
+    userId: 2,
+    like: 23,
+    comment: 9,
+  },
+  {
+    id: 3,
+    desc: "Love For All.",
+    photo: "assets/post/sudan3.jpg",
+    date: "45 mins ago",
+    userId: 3,
+    like: 50,
+    comment: 90,
+  }
+];
